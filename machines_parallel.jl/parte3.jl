8d587400c44e6654cf1efb0ccf80a057d6d124f2
GervisonRenan <renancabral280@gmail.com>
2024-12-10 17:47:16 -0300

    Delete machines_parallel.jl/parte3.jl

--- machines_parallel.jl/parte3.jl
+++
@@ -1,8 +0,0 @@
-include("parte1.jl")
-include("parte2.jl")
-
-
-address = "C:/Users/gervi/OneDrive/Área de Trabalho/progr/1011 (1).txt"
-instance = UnPM.open_instance(address)
-UnPM.test_file(instance::Main.UnPM.Instanceupm, m::Int, n::Int)
-save_solution(Cmax, x, model)
